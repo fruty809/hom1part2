eca57f79d5b32c86bcacd0d351d10279845baff2
fruty809 <115156929+fruty809@users.noreply.github.com>
2023-04-30 14:35:14 +0600

    Update SecondActivity.kt

--- app/src/main/java/com/example/a6mon1lesson/SecondActivity.kt
+++ app/src/main/java/com/example/a6mon1lesson/SecondActivity.kt
@@ -5,28 +5,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.a6mon1lesson.databinding.ActivitySecondBinding
 
+
 class SecondActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySecondBinding
+    lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initClicker()
+        val text = intent.getStringExtra(MA_SA_TEXT)
+        binding.edit1.setText(text)
+        initListener()
+
     }
-    private fun initClicker() {
-        val text = intent.getStringExtra("result")
-        with(binding) {
-            edText.setText(text)
-            btnBack.setOnClickListener {
-                val resultText = edText.text.toString().trim()
-                if (text?.isNotEmpty() == true){
-                    val intent = Intent().apply {
-                        putExtra("result",resultText)
-                    }
-                    setResult(RESULT_OK,intent)
-                }
-                finish()
-            }
+
+    private fun initListener() {
+        binding.btn1.setOnClickListener {
+            intent.putExtra(MA_SA_TEXT, binding.edit1.text.toString())
+            setResult(RESULT_OK, intent)
+            finish()
         }
     }
 }
