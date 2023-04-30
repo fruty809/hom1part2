fcb302b839fec429024d3615b0bed4aed8d18327
fruty809 <115156929+fruty809@users.noreply.github.com>
2023-04-30 14:34:33 +0600

    Update MainActivity.kt

--- app/src/main/java/com/example/a6mon1lesson/MainActivity.kt
+++ app/src/main/java/com/example/a6mon1lesson/MainActivity.kt
@@ -4,42 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.a6mon1lesson.databinding.ActivityMainBinding
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.mon6hom12.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val activityLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                binding.edit.setText(result.data?.getStringExtra(MA_SA_TEXT))
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initClicker()
+        initListener()
+
     }
 
-    private fun initClicker() {
-        with(binding) {
-            btnConfirm.setOnClickListener {
-                val text = edText.text.toString().trim()
-                if (text.isEmpty()) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        getString(R.string.cannot_empty),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                } else {
-                    val intent = Intent(this@MainActivity, SecondActivity::class.java).apply {
-                        putExtra("result", text)
-                    }
-                    startActivityForResult(intent, 1)
-                }
+    private fun initListener() {
+        binding.btn.setOnClickListener{
+            if (binding.edit.text.isEmpty()){
+                Toast.makeText(this, "Вы не ввели текст", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra(MA_SA_TEXT,binding.edit.text.toString())
+                activityLauncher.launch(intent)
             }
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1 && resultCode == RESULT_OK){
-            val text = data?.getStringExtra("result")
-            binding.edText.setText(text)
-        }
+    companion object{
+        const val MA_SA_TEXT = "MA_SA_TEXT"
     }
 }
